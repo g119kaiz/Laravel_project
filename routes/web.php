0f7be5e7587f98daa69d3dc7d2369a4a04edd97e
g119kaiz <g520119x@mails.cc.ehime-u.ac.jp>
2021-12-09 07:09:40 +0900

    画像の投稿機能を作成(途中)

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,9 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/posts', 'PostController@index');
+Route::get('/', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
+Route::get('/posts/{post}', 'PostController@show');
+Route::post('/posts', 'PostController@store');
